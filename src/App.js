2b57a7b348c1afb3a587a225d999a1b4b046f55b
Gemma Barrera <lagemyo@gmail.com>
2019-08-11 11:56:39 +0200

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
-    <h1>our current app</h1>
-  );
+  return <React.Fragment>
+    <Navbar/>
+  </React.Fragment>
 }
 
 export default App;
