09bd72502c32a0d83b96a86a7c4af5e901541259
Gemma Barrera <lagemyo@gmail.com>
2019-08-15 16:07:32 +0200

    continue with tourlist: showing data

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,10 +3,17 @@ import './tour.scss'
 
 export default class Tour extends Component {
     render() {
+        const { id, city, img, name, info } = this.props.tour;
+        const { removeTour } = this.props;
         return (
-            <div>
-                Hello from Tour
-            </div>
+            <article className="tour">
+                <div className="img-container">
+                    <img src={img} alt="image of the tour"/>
+                    <span className="close-btn">
+                        
+                    </span>
+                </div>
+            </article>
         )
     }
 }
--- src/components/TourList/index.js
+++ src/components/TourList/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import Tour from '../Tour';
+import './tourlist.scss';
+import {tourData} from '../../tourData';
 
 export default class TourList extends Component {
+    state = {
+        tours: tourData,
+    }
+    removeTour = id => {
+        console.log(id);
+    }
     render() {
+        const { tours } = this.state;
+        console.log(this.state.tours);
         return (
-            <div>
-              <Tour/>
-            </div>
+            <section className="tourlist">
+                {
+                    tours.map(tour => (<Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>))
+                }
+            </section>
         )
     }
 }
+
