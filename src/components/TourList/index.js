ac985369c3083a371835671badd9600f4d91512e
Gemma Barrera <lagemyo@gmail.com>
2019-08-15 15:19:58 +0200

    startig with tour and tourlist components

---
+++ src/components/TourList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Tour from '../Tour';
+
+export default class TourList extends Component {
+    render() {
+        return (
+            <div>
+              <Tour/>
+            </div>
+        )
+    }
+}
