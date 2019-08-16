414158414ca0164bcfc15aefaef335d73e7d5a58
Gemma Barrera <lagemyo@gmail.com>
2019-08-16 09:21:08 +0200

    added functionality for hidding and showing the info about the tour, by clicking the icon

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import './tour.scss';
 
 export default class Tour extends Component {
+    state = {
+        showInfo: false
+    }
+    handleInfo = () => {
+        this.setState({
+            showInfo: !this.state.showInfo
+        })
+    }
     render() {
         const { id, city, img, name, info } = this.props.tour;
         const { removeTour } = this.props;
@@ -17,11 +25,11 @@ export default class Tour extends Component {
                     <h3>{city}</h3>
                     <h3>{name}</h3>
                     <h5>info{""}
-                        <span>
+                        <span onClick= {this.handleInfo}>
                             <i className="fas fa-caret-square-down" />
                         </span>
                     </h5>
-                    <p>{info}</p>
+                    {this.state.showInfo && <p>{info}</p>}
                 </div>
             </article>
         )
